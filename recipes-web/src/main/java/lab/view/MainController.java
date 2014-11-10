9e13efd7c1cc3dbe557c1d8b9db07af5d660e9ac
Jean-francois <jeanmfrancois@hotmail.com>
2014-11-10 05:23:54 -0500

    Update POMs and Remove Core Content Included in Structure

--- recipes-web/src/main/java/lab/view/MainController.java
+++ recipes-web/src/main/java/lab/view/MainController.java
@@ -1,9 +1,5 @@
 package lab.view;
 
-import java.io.Serializable;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 
 /**
  * MainController (description of class)
@@ -15,40 +11,14 @@ import javax.faces.bean.ViewScoped;
  * @author Jean-francois Nepton
  * @version %I%, %G%
  * @since 1.0
+ * @ManagedBean
+ * @ViewScoped public class MainController implements Serializable { private
+ *             static final long serialVersionUID = 3973801993975443027L;
+ *             private String name; private String primeName; private String
+ *             richName; public String getName() { return name; } public void
+ *             setName(String name) { this.name = name; } public String
+ *             getPrimeName() { return primeName; } public void
+ *             setPrimeName(String primeName) { this.primeName = primeName; }
+ *             public String getRichName() { return richName; } public void
+ *             setRichName(String richName) { this.richName = richName; } }
  */
-@ManagedBean
-@ViewScoped
-public class MainController implements Serializable {
-
-	private static final long serialVersionUID = 3973801993975443027L;
-
-	private String name;
-
-	private String primeName;
-
-	private String richName;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPrimeName() {
-		return primeName;
-	}
-
-	public void setPrimeName(String primeName) {
-		this.primeName = primeName;
-	}
-
-	public String getRichName() {
-		return richName;
-	}
-
-	public void setRichName(String richName) {
-		this.richName = richName;
-	}
-}
